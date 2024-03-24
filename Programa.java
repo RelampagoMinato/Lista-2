01b6ffdc0c89ac7e16b9dc6c81f87872670186de
RelampagoMinato <julioandreguedes@gmail.com>
2024-03-24 17:59:32 -0300

    Exe 13

--- Programa.java
+++ Programa.java
@@ -12,8 +12,8 @@ public class Programa {
         //Exercicio09.executar();
         //Exercicio10.executar();
         //Exercicio11.executar();
-        Exercicio12.executar();
-        //Exercicio13.executar();
+        //Exercicio12.executar();
+        Exercicio13.executar();
         //Exercicio14.executar();
         //Exercicio15.executar();
          
